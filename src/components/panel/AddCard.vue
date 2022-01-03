fdafcb0fb0637ca3b16b0964dd0bb6418301f49c
huuhghhgyg <huuhghhgyg@outlook.com>
2022-01-03 16:17:17 +0800

    排除项目

--- src/components/panel/AddCard.vue
+++ src/components/panel/AddCard.vue
@@ -1,10 +1,13 @@
 <template>
-  <!-- 添加项目的卡片 -->
-  <button class="m-2 p-4 rounded-2xl bg-blue-100 text-blue-400 hover:bg-blue-200 hover:text-blue-500 active:bg-blue-300 transition focus:ring focus:ring-blue-300 ring-offset-2">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <!-- 添加项目的按钮 -->
+    <button
+      class="fixed bottom-2 right-2 flex m-2 p-4 rounded-2xl bg-blue-500 text-white hover:bg-blue-600 transition items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
       </svg>
-  </button>
+      <p class="pl-2">添加项目</p>
+    </button>
 </template>
 
 <script>
@@ -12,7 +15,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
